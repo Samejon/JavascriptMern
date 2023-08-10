873a45a252a382fdebefc8652c1f0546cca15caa
Samejon <example@gmail.com>
2023-08-10 06:06:54 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,183 @@
+"use strict"
+//alert("Hello Samejon");
+//let number = 10;
+//const borderColor="red";
+
+
+//number=20;
+//console.log(number);
+
+//const person={
+//    name:"Samar",
+//};
+//person.name="Yusuf";
+//console.log(person);
+
+
+
+       //number
+//let number=5.6; //Data Type=Number
+//console.log(-8/0);   //infinity vheksiz
+//console.log("Samar"*8); //NAN
+
+      //String
+//const clientName="Samar";
+
+
+      //Boolean
+//const isMarried=true;
+
+    //null bulmagan narsani chaqirsa
+    //console.log(clientAge);
+
+    //  undefined  bor lekin qiymati yuqni chaqirsa
+  //let und;
+ // console.log(und);
+
+
+      //object
+//const theif ={
+  //  age:30,
+    //jacket:"black",
+    //isLong: false,
+//};
+//console.log(theif.age);
+
+
+
+     //array
+//                0      1       2       3   4     5
+//const colors = ["red","black","green",   10, [],  {}];
+//console.log(colors[0]);
+
+
+//Foydalanuvchi bilan muloqot
+
+//alert("Hello javascript")
+
+//const place=confirm("Are you here");     // Ha yoki yud javob olish
+//console.log(place);
+
+//Foydalanuvchiga savol berib string javob olish
+ //const currentPlace=prompt("Where are you","")   
+ //console.log(currentPlace);
+
+//Foydalanuvchiga savol berib Number javob olish
+//const age=+prompt("how old are you");
+//console.log(typeof age);
+
+
+//Foydalanuvchiga savol berib  javobini bir joyga jamlash
+
+//const favoruteColor=[];
+//favoruteColor[0]=prompt("What's your favorite color #1", "");
+//favoruteColor[1]=prompt("What's your favorite color #2", "");
+//favoruteColor[2]=prompt("What's your favorite color #3", "");
+//console.log(favoruteColor);
+
+
+//////Interpalatsiya
+
+//const chanel=prompt ("What's your favorite youtuber","");
+//console.log(`https://youtube.com/${chanel}`);
+
+
+//////////Operatorlar
+
+//console.log(6+ +"9");
+
+
+///incr, decr
+//let incr=10;
+//let decr=10
+//console.log(incr++);
+//console.log(decr--);
+
+
+//console.log(10%3);    /// % Bulganda qoldiq chiqarish
+//console.log(5*5==="25");   //=== tuliq proverka qilish malumot turlari bilan
+
+
+  //////&&=va   ||=yoki
+//const isAge=true;
+//const isClose=true;
+//const isChesked=false;
+//console.log(isAge && isClose && !isChesked);  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
